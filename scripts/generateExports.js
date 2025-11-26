7ee9f50513915039f2c837b577e35fa1b2ce7123
ChakornK <chakornk2007@gmail.com>
2025-11-25 23:49:54 -0800

    fix(icons): make svg width/height matcher non-greedy

--- scripts/generateExports.js
+++ scripts/generateExports.js
@@ -12,7 +12,7 @@ const reactTemplate = (fileName, svg) => {
   for (const [find, replace] of Object.entries(replaceMap)) {
     formattedSvg = formattedSvg.replaceAll(find, replace);
   }
-  formattedSvg = formattedSvg.replace(/width=".+" height=".+"/, "width={size} height={size}");
+  formattedSvg = formattedSvg.replace(/width=".+?" height=".+?"/, "width={size} height={size}");
 
   const componentName = `Icon${fileName
     .replace(".svg", "")
